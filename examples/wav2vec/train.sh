735a6bd6501ff0c38e0ba2a962374c61e383a6e0
Vaibhav Singh <sivaibhav@google.com>
2020-05-14 00:43:29 +0000

    minor edits for debug

---
+++ examples/wav2vec/train.sh
@@ -0,0 +1,30 @@
+/home/sivaibhav/xla/scripts/debug_run.py \
+ --tidy --outfile /tmp/debug_run.tar.gz -- python -u \
+ train.py /home/sivaibhav/fairseq/wav2vec/data \
+--save-dir /home/sivaibhav/wav2vec/model \
+--num-workers 6 \
+--max-update 400 \
+--save-interval 1 \
+--no-epoch-checkpoints \
+--arch wav2vec \
+--task audio_pretraining \
+--lr 1e-06 \
+--min-lr 1e-09 \
+--optimizer adam \
+--max-lr 0.005 \
+--lr-scheduler cosine \
+--conv-feature-layers '[(512, 10, 5), (512, 8, 4), (512,4, 2), (512, 4, 2), (512, 4, 2), (512, 1, 1), (512, 1, 1)]' \
+--conv-aggregator-layers '[(512, 2, 1), (512, 3, 1), (512, 4, 1), (512, 5, 1), (512, 6, 1), (512, 7, 1), (512, 8,1) , (512, 9, 1), (512, 10, 1), (512, 11, 1), (512, 12, 1), (512, 13, 1)]' \
+--skip-connections-agg \
+--residual-scale 0.5 \
+--log-compression \
+--warmup-updates 500 \
+--warmup-init-lr 1e-07 \
+--criterion binary_cross_entropy \
+--num-negatives 10 \
+--max-sample-size 150000 \
+--skip-invalid-size-inputs-valid-test \
+--input_shapes 1x150000 \
+--disable-validation \
+--metrics_debug
+
